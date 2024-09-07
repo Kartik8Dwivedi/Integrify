1e3c4029965d58f038235acb18f98724cd07415a
Kartik <codewithme.kartik@gmail.com>
2024-09-08 02:50:57 +0530

    added middleware

--- backend/src/middlewares/middleware.ts
+++ backend/src/middlewares/middleware.ts
@@ -1,6 +1,17 @@
 import { NextFunction, Request, Response } from "express";
+import { JWT_PASSWORD } from "../config";
+import jwt from 'jsonwebtoken';
 
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    console.log("auth middleware");
+    const token = req.headers.authorization as unknown as string;
+
+    try {
+        const payload = jwt.verify(token, JWT_PASSWORD);
+        // @ts-ignore
+        req.id = payload.id;
+        next();
+    } catch (error) {
+        return res.status(401).json({error: "Unauthorized"});
+    }
 }
